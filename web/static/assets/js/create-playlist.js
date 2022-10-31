8afa244ac4d59325ef32fe93fed569891713daa3
Kaulitz Guimarães Oliveira <kaulitzguimaraes@hotmail.com>
2022-10-30 22:47:35 -0300

    reasonable version for frontend

--- web/static/assets/js/create-playlist.js
+++ web/static/assets/js/create-playlist.js
@@ -1,7 +1,7 @@
 
 document.getElementById("createPlaylist").addEventListener("click",
 async () => {
-    document.getElementById("embededPlaylistDiv").style = ""
+    document.getElementById("embededPlaylistDiv").style = "display: none;"
     let track_name = document.getElementById("trackName").value;
     let artist = document.getElementById("artist").value;
     console.log(track_name)
@@ -20,7 +20,10 @@ async () => {
     await fetchPromise.then((response => response.json())).then(
         (responseJSON) =>{
            document.getElementById("embededPlaylist").src =`https://open.spotify.com/embed/playlist/${responseJSON['id']}?utm_source=generator&theme=0`
-           document.getElementById("embededPlaylistDiv").style = ""
+          setTimeout( async ()=>{
+              document.getElementById("embededPlaylistDiv").style = ""
+          }, 10000)
+
         }
     )
 }
